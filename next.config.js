6614dce92746ad5a458e337b03d4f2d0aeaf9642
juan <jpredcoin@gmail.com>
2024-08-01 21:13:18 -0500

    avanzando con circulo en movimiento,falta centrarlo

--- next.config.js
+++ next.config.js
@@ -1,12 +1,12 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-    images: {
-      // Si utilizas imágenes locales, esta configuración es suficiente.
-      // Puedes agregar más configuraciones si las necesitas.
-      domains: ['bafybeifgvjz7fx7q7ubbbos32ffliiu7c7lsykcsdkvcmitjfkt3gksvge.ipfs.dweb.link'], // Reemplaza con dominios permitidos si usas imágenes externas
-    },
-    reactStrictMode: true, // Esto es opcional, pero recomendado para detectar problemas en desarrollo
-  };
-  
-  module.exports = nextConfig;
-  
+  images: {
+    domains: [
+      'bafybeifgvjz7fx7q7ubbbos32ffliiu7c7lsykcsdkvcmitjfkt3gksvge.ipfs.dweb.link',
+      'chocolate-legislative-lamprey-152.mypinata.cloud', // Agrega el nuevo dominio aquí
+    ],
+  },
+  reactStrictMode: true,
+};
+
+module.exports = nextConfig;
--- src/components/ui/Photo.tsx
+++ src/components/ui/Photo.tsx
@@ -1,4 +1,5 @@
 "use client";
+import { reverse } from "dns";
 import { delay, motion } from "framer-motion";
 import Image from "next/image";
 
@@ -12,15 +13,17 @@ const Photo = () => {
           transition: { delay: 2, duration: 0.4, ease: "easeIn" },
         }}
       >
+        {/*image*/}
         <motion.div
-        initial={{ opacity: 0 }}
-        animate={{
-          opacity: 1,
-          transition: { delay: 2.4, duration: 0.4, ease: "easeInOut" },
-        }}       
-        className="w-[298px] h-[298px] xl:w-[498px] xl:h-[498px] mix-blend-lighten">
+          initial={{ opacity: 0 }}
+          animate={{
+            opacity: 1,
+            transition: { delay: 2.4, duration: 0.4, ease: "easeInOut" },
+          }}
+          className="w-[298px] h-[298px] xl:w-[498px] xl:h-[498px] mix-blend-lighten absolute"
+        >
           <Image
-            src="https://bafybeifgvjz7fx7q7ubbbos32ffliiu7c7lsykcsdkvcmitjfkt3gksvge.ipfs.dweb.link/"
+            src="https://chocolate-legislative-lamprey-152.mypinata.cloud/ipfs/QmRTw6jDucssqbcy2W947KXAgcRsu38xf9fSC5d4hecEMV"
             priority
             quality={100}
             fill
@@ -28,6 +31,34 @@ const Photo = () => {
             className="object-contain"
           />
         </motion.div>
+
+        {/*circle*/}
+        <motion.svg
+          className="w-[300px] xl:w-[560px] h-[300px] xl:h-[506]"
+          fill="transparent"
+          viewBox="0 0 506 506"
+          xmlns="http://www.pablodrum.xyz"
+        >
+          <motion.circle
+            cx="253"
+            cy="253"
+            r="250"
+            stroke="#00ff99"
+            strokeWidth="4"
+            strokeLinecap="round"
+            strokeLinejoin={"round"}
+            initial={{strokeDasharray: "24 10 0 0"}}
+            animate={{
+              strokeDasharray:[ "15 120 25 25" ,"16 25 92 72","4 250 22 22"],
+              rotate:[120,360],              
+            }}
+            transition={{
+              duration:20,
+              repeat:Infinity,
+              repeatType:"reverse",
+            }}
+          />
+        </motion.svg>
       </motion.div>
     </div>
   );
