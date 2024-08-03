6aff40c80771881381ed04cd77cc3335fb76d2f4
juan <jpredcoin@gmail.com>
2024-08-03 09:57:33 -0500

    avanzando en resume2

--- src/app/resume/page.jsx
+++ src/app/resume/page.jsx
@@ -50,7 +50,7 @@ const about = {
 //experience data
 const experience = {
   icon: "/src/public/drum.png",
-  title: "Experience",
+  title: " My Experience",
   description:
     "Lorem ipsum dolor sit amet consectetur adipisicing elit. Recusandae voluptas dolorem architecto tempora numquam tempore reprehenderit non.",
   items: [
@@ -183,29 +183,53 @@ const Resume = () => {
           <div className="min-h-[70vh] w-full">
             {/*experience */}
             <TabsContent value="experience" className="w-full">
-              experience
+              <div className="flex flex-col gap-[30px] text-center xl:text-left">
+                <h3 className="text-4xl font-bold">{experience.title}</h3>
+                <p className="max-w-[600px] text-white/60 mx-auto xl:mx-0">
+                  {experience.description}
+                </p>
+                <ScrollArea className="h-[400px]">
+                  <ul className="grid grid-cols-1 lg:grid-cols-2 gap[30px]">
+                    {experience.items.map((item, index) => {
+                      return (
+                        <li
+                          key={index}
+                          className="bg-[#41416e] h-[184px] py-6 px-10 rounded-xl flex flex-col justify-center items-center lg:items-start gap-1"
+                        >
+                          <span>{item.duration}</span>
+                          <h3>{item.position}</h3>
+                          <div>
+                            {/*dot */}
+                            <span></span>
+                            <p>{item.company}</p>
+                          </div>
+                        </li>
+                      );
+                    })}
+                  </ul>
+                </ScrollArea>
+              </div>
             </TabsContent>
 
-             {/*education */}
-             <TabsContent value="education" className="w-full">
+            {/*education */}
+            <TabsContent value="education" className="w-full">
               education
             </TabsContent>
 
-             {/*skills */}
-             <TabsContent value="skills" className="w-full">
+            {/*skills */}
+            <TabsContent value="skills" className="w-full">
               skills
             </TabsContent>
 
-             {/*projects */}
-             <TabsContent value="projects" className="w-full">
+            {/*projects */}
+            <TabsContent value="projects" className="w-full">
               projects
             </TabsContent>
-            
-             {/*about */}
+
+            {/*about */}
             <TabsContent value="aboutme" className="w-full">
               about me
             </TabsContent>
-
           </div>
         </Tabs>
       </div>
